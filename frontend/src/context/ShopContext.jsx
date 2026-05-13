865ff973a057ccb390d6fa30ce35cac9ddb621a2
suraj project <sahoosuraj744@gmail.com>
2026-05-13 20:08:19 +0530

    feat: add navigation to checkout in Cart component and include navigate in ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "₹";
@@ -8,6 +9,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems,setCartItems]=useState({});
+  const navigate=useNavigate();
   const addToCart=async(itemId,size)=>{
     if(!size){
       toast.error('Select Product Size');
@@ -75,7 +77,8 @@ const ShopContextProvider = (props) => {
     cartItems,addToCart,
     getCartCount,
     updateQuantity,
-    getCartAmount
+    getCartAmount,
+    navigate
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -5,7 +5,8 @@ import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems,updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity,navigate } =
+    useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
   useEffect(() => {
     const tempData = [];
@@ -52,12 +53,33 @@ const Cart = () => {
                       {currency}
                       {productData.price}
                     </p>
-                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                      {item.size}
+                    </p>
                   </div>
                 </div>
               </div>
-             <input onChange={(e)=>e.target.value ==='' || e.target.value==='0' ?null :updateQuantity(item._id,item.size,Number(e.target.value))} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number" min={1} defaultValue={item.quantity} />
-             <img onClick={()=>updateQuantity(item._id,item.size,0)}  className="w-4 mr-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt="" />
+              <input
+                onChange={(e) =>
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value),
+                      )
+                }
+                className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+              />
+              <img
+                onClick={() => updateQuantity(item._id, item.size, 0)}
+                className="w-4 mr-4 sm:w-5 cursor-pointer"
+                src={assets.bin_icon}
+                alt=""
+              />
             </div>
           );
         })}
@@ -65,9 +87,12 @@ const Cart = () => {
 
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
-          <CartTotal/> </div>
+          <CartTotal />
+           <div className="w-full text-end ">
+            <button onClick={()=>navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">PROCEED TO CHECKOUT</button>
+           </div>
+        </div>
       </div>
-      
     </div>
   );
 };
