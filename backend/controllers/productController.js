fa4c72f2d1ce3189a2bbabfa0f6904caa4e15d0b
suraj project <sahoosuraj744@gmail.com>
2026-05-14 22:56:15 +0530

    fix: enhance product removal functionality and improve error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 //function for add products
-import {v2 as cloudinary} from 'cloudinary'
-import Product from '../models/productModel.js'
+import { v2 as cloudinary } from "cloudinary";
+import Product from "../models/productModel.js";
 const addProduct = async (req, res) => {
   try {
     const {
@@ -12,50 +12,66 @@ const addProduct = async (req, res) => {
       sizes,
       bestSeller,
     } = req.body;
-    const image1 =req.files.image1 && req.files.image1[0];
-    const image2 =req.files.image2 &&  req.files.image2[0];
-    const image3 =req.files.image3 &&  req.files.image3[0];
-    const image4 =req.files.image4 &&  req.files.image4[0];
-    const images=[image1,image2,image3,image4].filter((item)=>item!=undefined)
-    let imagesUrl=await Promise.all(
-      images.map(async(item)=>{
-        let result=await cloudinary.uploader.upload(item.path,{resource_type:'image'})
-        return result.secure_url
-      })
-    )
-     const productData={
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item != undefined,
+    );
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
+    const productData = {
       name,
       description,
       category,
-      price:Number(price),
+      price: Number(price),
       subCategory,
-      bestSeller:bestSeller==='true'?true:false,
-      sizes:JSON.parse(sizes),
-      image:imagesUrl,
-      date:Date.now()
-     }
-     console.log(productData);
-     const product=new Product(productData)
-     await product.save();
-   
-     return res.json({success:true,message:"Product added successfully"})
+      bestSeller: bestSeller === "true" ? true : false,
+      sizes: JSON.parse(sizes),
+      image: imagesUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+    const product = new Product(productData);
+    await product.save();
+
+    return res.json({ success: true, message: "Product added successfully" });
   } catch (error) {
     console.log(error);
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
   }
 };
 //function for list products
 const listProducts = async (req, res) => {
   try {
-    const products=await Product.find({});
-    res.json({success:true,products});
+    const products = await Product.find({});
+    res.json({ success: true, products });
   } catch (error) {
     console.log(error);
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
   }
 };
 //function for remove products
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+     const deleteProduct=await Product.findByIdAndDelete(req.body._id);
+     if(!deleteProduct){
+      return res.json({success:false,message:"Product doesnot found"})
+     }
+    
+    res.json({ success: true, message: "Product removed successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 //function for single product info
 const singleProduct = async (req, res) => {};
 export { addProduct, listProducts, removeProduct, singleProduct };
