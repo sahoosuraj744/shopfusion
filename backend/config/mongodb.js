1103dbeb6bbd2b842f0251dc89665a49e8ad30e0
suraj project <sahoosuraj744@gmail.com>
2026-05-14 12:38:31 +0530

    feat: implement MongoDB connection setup in mongodb.js and integrate it in server.js

---
+++ backend/config/mongodb.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const connectDB=async()=>{
+    mongoose.connection.on('connected',()=>{
+        console.log("Db Connected")
+    })
+     await mongoose.connect(`${process.env.MONGODB_URI}/E-Commerce`)
+}
+export default connectDB;
