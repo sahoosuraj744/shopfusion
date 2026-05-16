c254582e2e5aa03024eeed5326a64c91b8504062
suraj project <sahoosuraj744@gmail.com>
2026-05-16 10:26:59 +0530

    fix: update error messages for unauthorized access in admin authentication middleware

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -3,16 +3,22 @@ const adminAuth = async (req, res, next) => {
   try {
     const { token } = req.headers;
     if (!token) {
-      return res.json({ success: false, message: "Not Authorized for Login" });
+      return res.json({
+        success: false,
+        message: "Not Authorized for Login Again",
+      });
     }
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-      return res.json({ success: false, message: "Not Authorized for Login Again" });
+      return res.json({
+        success: false,
+        message: "Not Authorized for Login Again",
+      });
     }
     next();
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message });
   }
 };
 export default adminAuth;
