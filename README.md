ba863b009fa95952ccb8cf51a4f5826b7f364b3a
suraj project <sahoosuraj744@gmail.com>
2026-05-16 20:43:59 +0530

    feat: update Navbar to accept setToken prop and pass token to routes in App component; add README file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Title
+# This is an e-commerce website where we buy products and sells our own products like dress for all type. It is for those people who want to buy online dresses and also want to sell online dresses
+# In this website we added features like
+# *  Online payment option available
+# Buy and sell product
+# sell and buy many products at one time
+## Tech stack :React,Node.js Express,Tailwind,Mongodb
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center  px-[4%] py-2  justify-between'>
        <img src={assets.shopfusionadmin} className='w-80 ' alt="" />
-        <button className='bg-gray-600 text-white px-5 py-2  sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm '>Logout</button>
+        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2  sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm '>Logout</button>
     </div>
   )
 }
