fbb83f090fbcfcae81439ad4ee5d24d817d63f12
suraj project <sahoosuraj744@gmail.com>
2026-05-16 21:02:28 +0530

    chore: remove backend folder

--- backend/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const connectDB=async()=>{
-    mongoose.connection.on('connected',()=>{
-        console.log("Db Connected")
-    })
-     await mongoose.connect(`${process.env.MONGODB_URI}/E-Commerce`)
-}
-export default connectDB;
