e065e0bb66f5fd309954e8d8881b649c32c81fc8
suraj project <sahoosuraj744@gmail.com>
2026-05-16 10:41:09 +0530

    feat: add React and Vite setup with initial assets and styles
    - Added React SVG asset for branding.
    - Added Vite SVG asset for branding.
    - Created global CSS variables and styles for light and dark themes.
    - Implemented main entry point for React application.
    - Configured Vite for React project.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,14 +12,14 @@ const addProduct = async (req, res) => {
       sizes,
       bestSeller,
     } = req.body;
-    const image1 = req.files.image1 && req.files.image1[0];
-    const image2 = req.files.image2 && req.files.image2[0];
-    const image3 = req.files.image3 && req.files.image3[0];
-    const image4 = req.files.image4 && req.files.image4[0];
-    // const image1 = req.files?.image1?.[0];
-    // const image2 = req.files?.image2?.[0];
-    // const image3 = req.files?.image3?.[0];
-    // const image4 = req.files?.image4?.[0];
+    // const image1 = req.files.image1 && req.files.image1[0];
+    // const image2 = req.files.image2 && req.files.image2[0];
+    // const image3 = req.files.image3 && req.files.image3[0];
+    // const image4 = req.files.image4 && req.files.image4[0];
+    const image1 = req.files?.image1?.[0];
+    const image2 = req.files?.image2?.[0];
+    const image3 = req.files?.image3?.[0];
+    const image4 = req.files?.image4?.[0];
     const images = [image1, image2, image3, image4].filter(
       (item) => item != undefined,
     );
