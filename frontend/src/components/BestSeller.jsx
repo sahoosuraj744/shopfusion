8ace918440e2932223a4b7f011e751142c51cf1b
suraj project <sahoosuraj744@gmail.com>
2026-05-18 21:54:48 +0530

    feat: add axios for API calls and update product fetching logic in ShopContext

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -7,9 +7,9 @@ const BestSeller = () => {
   const { products } = useContext(ShopContext);
   const [bestSeller, setBestSeller] = useState([]);
   useEffect(() => {
-    const bestProduct = products.filter((item) => item.bestseller);
+    const bestProduct = products.filter((item) => (item.bestSeller === true));
     setBestSeller(bestProduct.slice(0, 5));
-  }, []);
+  }, [products]);
   return (
     <div className="my-10">
       <div className="text-center text-3xl py-8">
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,16 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "₹";
   const delivery_fee = 10;
+  const backend_url=import.meta.env.VITE_BACKEND_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems,setCartItems]=useState({});
+  const [products, setProducts] = useState([]);
   const navigate=useNavigate();
   const addToCart=async(itemId,size)=>{
     if(!size){
@@ -66,6 +68,23 @@ const ShopContextProvider = (props) => {
       }
       return totalAmount;
     }
+  
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backend_url + "/api/product/list");
+      if (response.data.success) {
+        setProducts(response.data.products);
+      }else{
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to fetch product data");
+    }
+  }
+  useEffect(() => {
+    getProductsData();
+  }, []);
   const value = {
     products,
     currency,
@@ -77,8 +96,8 @@ const ShopContextProvider = (props) => {
     cartItems,addToCart,
     getCartCount,
     updateQuantity,
-    getCartAmount,
-    navigate
+    getCartAmount,navigate,
+    backend_url
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
